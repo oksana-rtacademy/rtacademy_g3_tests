041eb1fa8bfdaf02f0412c8fdcca7ab43b117470
oksana <oksana.alekseienko@gmail.com>
2022-08-05 14:28:12 +0300

    по іншій темі

---
+++ mocha/09/users.js
@@ -0,0 +1,9 @@
+const { expect, assert } = require( 'chai' );
+
+describe( 'Users', function()
+{
+    it( 'Перегляд списку користувачів', function()
+    {
+        assert.equal( [ 1, 2, 3 ].indexOf( 4 ), -1 );
+    } );
+} );
