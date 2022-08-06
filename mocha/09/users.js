b1cacbc32b3b9cd2b65f95e4803ff686f060934d
oksana <oksana.alekseienko@gmail.com>
2022-08-06 20:31:57 +0300

    mocha

--- mocha/09/users.js
+++ mocha/09/users.js
@@ -1,9 +1,57 @@
+'use strict';
+
 const { expect, assert } = require( 'chai' );
+const chai = require( "chai" );
+
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+require( 'dotenv' ).config();
+
+const { faker } = require( '@faker-js/faker' );
+
+const TOKEN = process.env.TOKEN;
 
 describe( 'Users', function()
 {
-    it( 'Перегляд списку користувачів', function()
+    describe( 'Users list', function()
     {
-        assert.equal( [ 1, 2, 3 ].indexOf( 4 ), -1 );
+        it( 'Перегляд списку користувачів', async () =>
+        {
+            const res = await chai.request( "http://api.blog.local" )
+                .get( "/users" )
+                .set( 'Accept',  'application/json' )
+                .set( 'Content-Type',  'application/json' )
+                .set( 'X-TOKEN',  TOKEN )
+            expect( res ).to.be.json;
+
+            console.log(res.body);
+        } );
+    } );
+
+    describe( 'Users add', function()
+    {
+        it( 'Додавання користувача', async () =>
+        {
+            let login = faker.word.noun();
+            let email = login + "@gmail.com";
+            let password = login + login;
+
+            const res = await chai.request( "http://api.blog.local" )
+                .post( "/users" )
+                .set( 'Accept',  'application/json' )
+                .set( 'Content-Type',  'application/json' )
+                .set( 'X-TOKEN',  '1SEkpnyDplN0DVqvRkgcRur6HKmg7TGahW3MF3RkirOIuuX3tzFlpU7f5IbUhFPFWtH21KQG3XzRMP9CnVbyPWiCWiJ4HYaDRhL3y955h7XByl7ZaNXIdmiuYAaBZcjM' )
+                .send(
+                {
+                    "login"    : login,
+                    "password" : password,
+                    "email"    : email,
+                    "lastname" : faker.name.lastName(),
+                    "firstname": faker.name.firstName(),
+                } );
+
+            expect( res ).to.be.json;
+        } );
     } );
 } );
