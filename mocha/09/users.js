655dbacbff2cdaa27190d7673fa862bd7e7f1602
oksana <oksana.alekseienko@gmail.com>
2022-08-14 08:50:45 +0300

    mocha

--- mocha/09/users.js
+++ mocha/09/users.js
@@ -16,42 +16,43 @@ describe( 'Users', function()
 {
     describe( 'Users list', function()
     {
-        it( 'Перегляд списку користувачів', async () =>
+        it( 'Має повернути список користувачів', async () =>
         {
-            const res = await chai.request( 'http://api.blog.local:80' )
+            const response = await chai.request( 'http://api.blog.local' )
                 .get( '/users' )
                 .set( 'Accept',  'application/json' )
                 .set( 'Content-Type',  'application/json' )
                 .set( 'X-TOKEN', '1SEkpnyDplN0DVqvRkgcRur6HKmg7TGahW3MF3RkirOIuuX3tzFlpU7f5IbUhFPFWtH21KQG3XzRMP9CnVbyPWiCWiJ4HYaDRhL3y955h7XByl7ZaNXIdmiuYAaBZcjM' )
-            expect( res ).to.be.json;
+            expect( response ).to.be.json;
 
-            console.log(res.body);
         } );
     } );
 
     describe( 'Users add', function()
     {
-        it( 'Додавання користувача', async () =>
+        it( 'Має додати користувача', async () =>
         {
             let login = faker.word.noun();
             let email = login + "@gmail.com";
             let password = login + login;
+            let lastname = faker.name.lastName();
+            let firstname = faker.name.firstName();
 
-            const res = await chai.request( 'api.blog.local' )
+            const response = await chai.request( 'api.blog.local' )
                 .post( '/users' )
                 .set( 'Accept',  'application/json' )
                 .set( 'Content-Type',  'application/json' )
                 .set( 'X-TOKEN', '1SEkpnyDplN0DVqvRkgcRur6HKmg7TGahW3MF3RkirOIuuX3tzFlpU7f5IbUhFPFWtH21KQG3XzRMP9CnVbyPWiCWiJ4HYaDRhL3y955h7XByl7ZaNXIdmiuYAaBZcjM' )
                 .send(
-                {
-                    "login"    : login,
-                    "password" : password,
-                    "email"    : email,
-                    "lastname" : faker.name.lastName(),
-                    "firstname": faker.name.firstName(),
-                } );
+                    {
+                        "login"    : login,
+                        "password" : password,
+                        "email"    : email,
+                        "lastname" : lastname,
+                        "firstname": firstname,
+                    } );
 
-            expect( res ).to.be.json;
+            expect( response ).to.be.json;
         } );
     } );
 } );
